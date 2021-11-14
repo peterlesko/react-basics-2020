54c063cf59a88f34a93df18f2edecbdf088e96ba
peterlesko <peterlesko@yahoo.com>
2021-11-14 19:57:17 +0000

    basic structure, data, Events basic

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71rDThWVwES._AC_UL200_SR200,200_.jpg',
+    title: 'The Stranger in the Lifeboat: A Novel',
+    author: 'Mitch Albom',
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71ki8f20R7S._AC_UL200_SR200,200_.jpg',
+    title: 'The Judge\'s List: A Novel',
+    author: 'John Grisham',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81Ta5Jce%2BeL._AC_UL200_SR200,200_.jpg',
+    title: 'Cloud Cuckoo Land: A Novel',
+    author: 'Anthony Doerr',
+  },
+]
